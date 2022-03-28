8a775163408bd4e7b0f0a475300f6925fcc354ac
Taliah Wharton <taliahwh23@yahoo.com>
2022-03-28 15:24:36 -0400

    tailwind font config

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="font-spratBlack  text-5xl mb-2">Black</h1>
+      <h1 className="font-spratBold text-5xl mb-2">Bold</h1>
+      <h1 className="font-spratRegular text-5xl mb-2">Regular</h1>
+      <p className="font-roboto text-5xl mb-2">Roboto</p>
+      {/* <p className="text-xl">Test body font style</p> */}
     </div>
   );
-}
+};
 
 export default App;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,18 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    fontFamily: {
+      roboto: ['Roboto', 'san-serif'],
+      spratBold: ['Sprat-Bold', 'serif'],
+      spratBlack: ['Sprat-Black', 'serif'],
+      spratRegular: ['Sprat-Regular', 'serif'],
+    },
     extend: {},
   },
   plugins: [],

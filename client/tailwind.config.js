fe4b24114f185c890ae672ebddde492e86848084
Taliah Wharton <taliahwh23@yahoo.com>
2022-03-29 11:44:18 -0400

    tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -9,11 +9,26 @@ module.exports = {
     },
     fontFamily: {
       roboto: ['Roboto', 'san-serif'],
+      robotoLight: ['Roboto-Light', 'san-serif'],
+      robotoRegular: ['Roboto-Regular', 'san-serif'],
       spratBold: ['Sprat-Bold', 'serif'],
       spratBlack: ['Sprat-Black', 'serif'],
       spratRegular: ['Sprat-Regular', 'serif'],
     },
-    extend: {},
+    extend: {
+      colors: {
+        background: '#fafafa',
+      },
+      maxWidth: {
+        '8xl': '85rem',
+      },
+      borderWidth: {
+        1: '1px',
+      },
+      textUnderlineOffset: {
+        12: '12px',
+      },
+    },
   },
   plugins: [],
 };

7f2e1881ced091585c337010e561b97af901a56b
Taliah Wharton <taliahwh23@yahoo.com>
2022-03-30 12:18:10 -0400

    blog screen

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import HomeScreen from './screens/HomeScreen';
+import BlogScreen from './screens/BlogScreen';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" exact element={<HomeScreen />} />
+          <Route path="/blog" exact element={<BlogScreen />} />
         </Routes>
       </BrowserRouter>
     </div>

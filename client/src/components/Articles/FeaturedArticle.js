153b29105e9e532ee63a63d9ea7db8b889032c85
Taliah Wharton <taliahwh23@yahoo.com>
2022-03-30 11:13:13 -0400

    responsive ui

--- client/src/components/Articles/FeaturedArticle.js
+++ client/src/components/Articles/FeaturedArticle.js
@@ -9,7 +9,7 @@ const FeaturedArticle = () => {
           src="https://sportshub.cbsistatic.com/i/r/2022/01/11/a886ee4f-011e-4deb-aca3-d209920410bc/thumbnail/1200x675/470eee4b9dbe3be529dee35e99bf0a05/jaylen-brown-jayson-tatum-celtics-getty.jpg"
           alt="Cover"
         />
-        <div className="flex justify-between pt-2 font-robotoLight text-sm">
+        <div className="hidden lg:flex justify-between pt-2 font-robotoLight text-sm">
           <div className="space-x-3 md:flex hidden">
             <p className="text-neutral-400">Feb 11</p>
             <p className="text-neutral-400">•</p>
@@ -21,6 +21,10 @@ const FeaturedArticle = () => {
         </div>
       </div>
 
+      <p className="text-neutral-600 text-xs md:text-sm block lg:hidden">
+        ⸻ <span className="pl-3">BY ELLEN JHOSH</span>
+      </p>
+
       <h1 className="text-xl md:text-2xl font-spratRegular ">
         Boston Celtics: Are They For Real or Pretenders?
       </h1>
@@ -28,7 +32,8 @@ const FeaturedArticle = () => {
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente error,
         necessitatibus, placeat to dk facere labore quia, quis atque laborum
       </p>
-      <div className="space-x-3 md:hidden flex text-xs md:text-sm font-robotoLight">
+
+      <div className="space-x-3 lg:hidden flex text-xs md:text-sm font-robotoLight">
         <p className="text-neutral-400">Feb 11</p>
         <p className="text-neutral-400">•</p>
         <p className="text-neutral-400">5 min read</p>

5448fc8fc769e749f21465f5f9ba18b65ed7516b
Taliah Wharton <taliahwh23@yahoo.com>
2022-04-08 13:27:22 -0400

    react-editor-js

---
+++ client/src/config/editorTools.js
@@ -0,0 +1,7 @@
+import Header from '@editorjs/header';
+
+export const EDITOR_JS_TOOLS = {
+  // NOTE: Paragraph is default tool. Declare only when you want to change paragraph option.
+  // paragraph: Paragraph
+  header: Header,
+};

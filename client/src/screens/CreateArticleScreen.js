94d57e3b6e29bc4c8efde162bf77591f8c4345c2
Taliah Wharton <taliahwh23@yahoo.com>
2022-04-08 10:48:40 -0400

    ckeditor5

--- client/src/screens/CreateArticleScreen.js
+++ client/src/screens/CreateArticleScreen.js
@@ -3,10 +3,12 @@ import ImageUploading from 'react-images-uploading';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
+// CKEditor Plugins
+// import { Heading } from '@ckeditor/ckeditor5-heading';
+
 import { BsFileEarmarkImage } from 'react-icons/bs';
 import { FiCheck } from 'react-icons/fi';
 import { MdCancel } from 'react-icons/md';
-// import { MdCancel } from 'react-icons/md';
 
 const CreateArticleScreen = () => {
   const [image, setImage] = useState([]);
@@ -121,21 +123,20 @@ const CreateArticleScreen = () => {
 
         <CKEditor
           editor={ClassicEditor}
-          config={editorConfiguration}
-          data="<p>Start writing your article..</p>"
+          data="<p>Hello from CKEditor 5!</p>"
           onReady={(editor) => {
             // You can store the "editor" and use when it is needed.
             console.log('Editor is ready to use!', editor);
           }}
           onChange={(event, editor) => {
             const data = editor.getData();
-            // console.log({ event, editor, data });
+            console.log({ event, editor, data });
           }}
           onBlur={(event, editor) => {
-            // console.log('Blur.', editor);
+            console.log('Blur.', editor);
           }}
           onFocus={(event, editor) => {
-            // console.log('Focus.', editor);
+            console.log('Focus.', editor);
           }}
         />
 

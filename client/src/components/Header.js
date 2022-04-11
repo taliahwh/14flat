6d6eaa252ee09b05ea45ae55a8343494272c7d51
Taliah Wharton <taliahwh23@yahoo.com>
2022-04-11 13:33:13 -0400

    header profile dropdown

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,6 +7,8 @@ import cancel from '../assets/cancel.png';
 
 const Header = () => {
   const [mobileOpen, setMobileOpen] = useState(false);
+  const [userOpen, setUserOpen] = useState(false);
+
   return (
     <div className="bg-background pb-8">
       <nav className="flex items-center justify-between h-20 max-w-8xl mx-auto relative z-50">
@@ -52,10 +54,74 @@ const Header = () => {
         </div>
 
         <div className="items-center hidden md:flex">
-          <Link to="/">
+          {/* <Link to="/">
             <img src={user} className="w-8 h-8" alt="user" />
-          </Link>
+          </Link> */}
+
+          {/* User Drowdown Dropdown */}
+          <div
+            className="text-black text-3xl cursor-pointer hidden md:flex"
+            onClick={() => setUserOpen(!userOpen)}
+          >
+            {userOpen ? (
+              <img src={cancel} className="w-4 h-4" alt="close menu" />
+            ) : (
+              <img src={user} className="w-7 h-7" alt="open menu" />
+            )}
+          </div>
+          {userOpen && (
+            <div className=" absolute top-full right-0 flex flex-col  w-1/5 h-[20rem] py-8 bg-background shadow-md">
+              <h1 className="font-roboto font-medium text-md text-center py-4 text-neutral-600">
+                Toni Miller
+              </h1>
+
+              {/* Border */}
+              <div className="border-t-1 border-x-neutral-500 pb-5"></div>
+
+              <div className="flex-1 flex flex-col items-center text-md space-y-4">
+                <NavLink
+                  to="/profile"
+                  className={({ isActive }) =>
+                    isActive
+                      ? 'underline underline-offset-12 font-roboto font-medium'
+                      : 'no-underline font-robotoLight'
+                  }
+                >
+                  Profile
+                </NavLink>
+                <NavLink
+                  to="/new-article"
+                  className={({ isActive }) =>
+                    isActive
+                      ? 'underline underline-offset-12 font-roboto font-medium'
+                      : 'no-underline font-robotoLight'
+                  }
+                >
+                  New Article
+                </NavLink>
+                <NavLink
+                  to="/saved-articles"
+                  className={({ isActive }) =>
+                    isActive
+                      ? 'underline underline-offset-12 font-roboto font-medium'
+                      : 'no-underline font-robotoLight'
+                  }
+                >
+                  Saved Articles
+                </NavLink>
+
+                <NavLink
+                  to="/logout"
+                  className="'underline underline-offset-12 font-roboto font-medium text-sm"
+                >
+                  Logout
+                </NavLink>
+              </div>
+            </div>
+          )}
         </div>
+
+        {/* Mobile Dropdown */}
         <div
           className="text-black text-3xl cursor-pointer md:hidden"
           onClick={() => setMobileOpen(!mobileOpen)}
@@ -68,7 +134,7 @@ const Header = () => {
         </div>
         {mobileOpen && (
           <div className=" absolute top-full left-0 flex flex-col  w-full h-screen py-8 bg-background lg:hidden">
-            <div className="flex-1 flex flex-col items-center text-lg space-y-8">
+            <div className="flex flex-col items-center text-lg space-y-8 pt-4 pb-8">
               <NavLink
                 to="/"
                 className={({ isActive }) =>
@@ -98,6 +164,53 @@ const Header = () => {
                 }
               >
                 Podcasts
+              </NavLink>
+            </div>
+
+            {/* Border */}
+            <div className="border-t-1 border-x-neutral-500 pb-5"></div>
+
+            <h1 className="font-roboto font-medium text-md text-center py-4 text-neutral-600">
+              Toni Miller
+            </h1>
+
+            <div className="flex-1 flex flex-col items-center text-md space-y-4">
+              <NavLink
+                to="/profile"
+                className={({ isActive }) =>
+                  isActive
+                    ? 'underline underline-offset-12 font-roboto font-medium'
+                    : 'no-underline font-robotoLight'
+                }
+              >
+                Profile
+              </NavLink>
+              <NavLink
+                to="/new-article"
+                className={({ isActive }) =>
+                  isActive
+                    ? 'underline underline-offset-12 font-roboto font-medium'
+                    : 'no-underline font-robotoLight'
+                }
+              >
+                New Article
+              </NavLink>
+              <NavLink
+                to="/saved-articles"
+                className={({ isActive }) =>
+                  isActive
+                    ? 'underline underline-offset-12 font-roboto font-medium'
+                    : 'no-underline font-robotoLight'
+                }
+              >
+                Saved Articles
+              </NavLink>
+
+              <NavLink
+                to="/logout"
+                className="'underline underline-offset-12 font-roboto font-medium text-sm"
+              >
+                Logout
               </NavLink>
             </div>
           </div>

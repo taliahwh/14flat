e29d17debd21826c4bc2ec9ba74dd3d0c9796606
Taliah Wharton <taliahwh23@yahoo.com>
2022-04-12 13:22:16 -0400

    redux setup

---
+++ client/src/store.js
@@ -0,0 +1,20 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from '@redux-devtools/extension';
+
+import { articleListReducer } from './reducers/articleReducers';
+
+const reducer = combineReducers({ articleList: articleListReducer });
+
+// Initial state when the redux store loads
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;

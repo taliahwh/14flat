cc9417d74d948c38b4b68d3080304e2b91506f7e
Taliah Wharton <taliahwh23@yahoo.com>
2022-04-13 16:05:26 -0400

    latest article state management

--- client/src/components/Articles/LatestArticle.js
+++ client/src/components/Articles/LatestArticle.js
@@ -1,32 +1,34 @@
 import React from 'react';
+import moment from 'moment';
+import readingTime from 'reading-time/lib/reading-time';
 
-const LatestArticle = () => {
+const LatestArticle = ({ article }) => {
+  const { text: readingLength } = readingTime(article.content);
+
   return (
     <>
       <div className="md:flex space-x-10 hidden">
         <img
           className="w-[19rem] h-[12rem] object-fit"
-          src="https://www.nbcsports.com/sites/rsnunited/files/article/hero/steph-curry-klay-thompson-warriors-Getty.jpg"
+          src={article.coverImage}
           alt="latest article"
         />
         <div className="flex flex-col space-y-2">
           <p className="text-neutral-600 text-xs pb-3 pt-1">
-            ⸻ <span className="pl-3">BY ELLEN JHOSH</span>
+            ⸻{' '}
+            <span className="pl-3">
+              BY {article.writtenBy.name.toUpperCase()}
+            </span>
           </p>
-          <h1 className="text-2xl font-spratRegular">
-            What's going on in Dub Nation?
-          </h1>
-          <p className="font-robotoLight text-sm">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure fuga,
-            molestiae in perspiciatis explicabo animi laboriosam quam ipsa
-            beatae quos neque facere, totam blanditiis facilis eius ab ipsam
-            repellendus aperiam.
-          </p>
+          <h1 className="text-2xl font-spratRegular">{article.title}</h1>
+          <p className="font-robotoLight text-sm">{article.excerpt}</p>
 
           <div className="flex space-x-3 text-xs pt-3">
-            <p className="text-neutral-400">Feb 11</p>
+            <p className="text-neutral-400">
+              {moment(article.createdAt).format('MMMM Do')}
+            </p>
             <p className="text-neutral-400">•</p>
-            <p className="text-neutral-400">5 min read</p>
+            <p className="text-neutral-400">{readingLength}</p>
           </div>
         </div>
       </div>
@@ -35,29 +37,30 @@ const LatestArticle = () => {
       <div className="flex flex-col w-full md:hidden">
         <img
           className="h-60 object-fit"
-          src="https://www.nbcsports.com/sites/rsnunited/files/article/hero/steph-curry-klay-thompson-warriors-Getty.jpg"
+          src={article.coverImage}
           alt="latest article"
         />
 
         <div className="flex justify-between pt-3 font-robotoLight text-sm">
           <div className="flex space-x-3 text-xs">
-            <p className="text-neutral-400">Feb 11</p>
+            <p className="text-neutral-400">
+              {' '}
+              {moment(article.createdAt).format('MMMM Do')}
+            </p>
             <p className="text-neutral-400">•</p>
             <p className="text-neutral-400">5 min read</p>
           </div>
           <p className="text-neutral-600 text-xs">
-            ⸻ <span className="pl-3">BY ELLEN JHOSH</span>
+            ⸻{' '}
+            <span className="pl-3">
+              BY {article.writtenBy.name.toUpperCase()}
+            </span>
           </p>
         </div>
 
-        <h1 className="text-2xl font-spratRegular py-3">
-          What's going on in Dub Nation?
-        </h1>
+        <h1 className="text-2xl font-spratRegular py-3">{article.title}</h1>
         <p className="font-robotoLight text-sm text-justify">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure fuga,
-          molestiae in perspiciatis explicabo animi laboriosam quam ipsa beatae
-          quos neque facere, totam blanditiis facilis eius ab ipsam repellendus
-          aperiam.
+          {article.excerpt}
         </p>
       </div>
     </>

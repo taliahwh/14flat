63d83348ffb037956805dad721c1cb988ee80e0d
Taliah Wharton <taliahwh23@yahoo.com>
2022-04-15 15:51:07 -0400

    featuredPodcasts, podcastDetails, and episodeDetails

--- client/src/components/Podcasts/PodcastCard.js
+++ client/src/components/Podcasts/PodcastCard.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
-const PodcastCard = () => {
+const PodcastCard = ({ show }) => {
   return (
     <div className="flex flex-col space-y-3 w-full md:w-64 pb-3">
       <img
-        className="w-full md:w-64 md:h-64"
-        src="https://cdn.theathletic.com/app/uploads/2021/10/18160943/NBA_SHOW2-copy-scaled.jpg"
-        alt="podcast"
+        className="w-full md:w-52 md:h-52 lg:w-64 lg:h-64"
+        src={show.images[0].url}
+        alt={show.name}
       />
       <div className="flex flex-col space-y-1 text-center">
-        <h1 className="font-spratRegular text-[22px]">The Athletic NBA Show</h1>
-        <p className="font-robotoLight">The Athletic</p>
+        <h1 className="font-spratRegular text-[22px] hover:underline">
+          {show.name}
+        </h1>
+        <p className="font-robotoLight text-sm">{show.publisher}</p>
       </div>
     </div>
   );

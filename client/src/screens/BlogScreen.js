fd84ff839f5e07abb0b64fb0463b33282b70d3a0
Taliah Wharton <taliahwh23@yahoo.com>
2022-04-26 10:12:08 -0400

    search bar

--- client/src/screens/BlogScreen.js
+++ client/src/screens/BlogScreen.js
@@ -21,6 +21,10 @@ const BlogScreen = () => {
     error: errorArticleList,
   } = useSelector((state) => state.articleList);
 
+  const handleSearch = (e) => {
+    e.preventDefault();
+  };
+
   useEffect(() => {
     dispatch(listArticles());
   }, [dispatch]);
@@ -34,60 +38,44 @@ const BlogScreen = () => {
 
       {/* Latest Articles Section */}
       <div className="pt-8 md:pt-12 pb-8">
-        <div className="flex w-full border-t-1 border-b-1 border-neutral-300 items-center justify-center space-x-4">
-          <NavLink
-            to="/blog"
-            className="font-spratRegular text-lg py-3 underline  hover:text-neutral-600"
-          >
-            View all
-          </NavLink>
-
-          <p className="text-xs font-spratRegular">or</p>
-
-          <div className="inline-block relative w-36">
-            <select className="font-spratRegular text-neutral-500 block appearance-none bg-background border-none  px-1 py-2 pr-8 focus:outline-none focus:shadow-outline">
-              <option className="font-spratRegular">Filter by Team</option>
-              <option value="atl">Atlanta Hawks</option>
-              <option value="bos">Boston Celtics</option>
-              <option value="bkn">Brooklyn Nets</option>
-              <option value="cha">Charlotte Hornets</option>
-              <option value="chi">Chicago Bulls</option>
-              <option value="cle">Cleveland Cavaliers</option>
-              <option value="dal">Dallas Mavericks</option>
-              <option value="den">Denver Nuggets</option>
-              <option value="det">Detroit Pistons</option>
-              <option value="gsw">Golden State Warriors</option>
-              <option value="hou">Houston Rockets</option>
-              <option value="ind">Indiana Pacers</option>
-              <option value="lac">Los Angeles Clippers</option>
-              <option value="lal">Los Angeles Lakers</option>
-              <option value="mem">Memphis Grizzlies</option>
-              <option value="mia">Miami Heat</option>
-              <option value="mil">Milwaukee Bucks</option>
-              <option value="min">Minnesota Timberwolves</option>
-              <option value="nop">New Orleans Pelicans</option>
-              <option value="nyk">New York Knicks</option>
-              <option value="okc">Oklahoma City Thunder</option>
-              <option value="orl">Orlando Magic</option>
-              <option value="phi">Philadelphia 76ers</option>
-              <option value="phx">Phoenix Suns</option>
-              <option value="por">Portland Trail Blazers</option>
-              <option value="sac">Sacramento Kings</option>
-              <option value="sas">San Antonio Spurs</option>
-              <option value="tor">Toronto Raptors</option>
-              <option value="uta">Utah Jazz</option>
-              <option value="was">Washington Wizards</option>
-            </select>
-            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-              <svg
-                className="fill-current h-4 w-4"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-              >
-                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-              </svg>
+        <div className="flex justify-center border-t-1  border-b-1 border-neutral-300">
+          <form onSubmit={handleSearch}>
+            <label
+              htmlFor="default-search"
+              className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300"
+            >
+              Search
+            </label>
+            <div className="relative">
+              <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <svg
+                  className="w-4 h-4 text-neutral-800"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  ></path>
+                </svg>
+              </div>
+              <input
+                type="search"
+                id="default-search"
+                className="w-[20rem] p-4 pl-9 text-sm text-neutral-800 border-none bg-background font-spratRegular focus:outline-none"
+                placeholder="Search by title or keyword..."
+                autoComplete="off"
+                required
+              />
+              <button type="submit" className="hidden ">
+                Search
+              </button>
             </div>
-          </div>
+          </form>
         </div>
 
         {/* Mobile Header View */}

75d747c13dc861cc8b1444adaa6ed9fed51b4c74
Taliah Wharton <taliahwh23@yahoo.com>
2022-04-26 15:46:59 -0400

    delete article feature

--- client/src/components/Articles/ViewWrittenArticle.js
+++ client/src/components/Articles/ViewWrittenArticle.js
@@ -1,39 +1,54 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import { FaThumbsUp } from 'react-icons/fa';
 import { IoMdEye } from 'react-icons/io';
 
-const ViewWrittenArticle = () => {
+// Actions
+import { deleteArticle } from '../../actions/articleActions';
+
+const ViewWrittenArticle = ({ article }) => {
+  const dispatch = useDispatch();
+
+  const id = article && article._id;
+
+  const handleDeleteArticle = () => {
+    dispatch(deleteArticle(id));
+  };
   return (
     <>
       <div className="lg:flex space-x-10 hidden">
         <img
-          className="w-[19rem] h-[12rem] object-fit"
-          src="https://www.nbcsports.com/sites/rsnunited/files/article/hero/steph-curry-klay-thompson-warriors-Getty.jpg"
+          className="w-[15rem] h-[12rem] object-fit"
+          src={article.coverImage}
           alt="latest article"
         />
+
         <div className="flex flex-col justify-between">
           <div className="flex flex-col space-y-4">
             <h4 className="text-neutral-500 text-xs font-roboto font-bold">
               APR 1, 2022
             </h4>
-            <h1 className="text-2xl font-spratRegular">
-              What's going on in Dub Nation?
-            </h1>
+            <Link to={`/blog/${article && article._id}`}>
+              <h1 className="text-2xl font-spratRegular hover:underline">
+                {article && article.title}
+              </h1>
+            </Link>
             <p className="font-robotoLight text-sm">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
-              fuga, molestiae in perspiciatis explicabo animi laboriosam quam
-              ipsa beatae quos neque facere, totam blanditiis facilis eius ab
-              ipsam repellendus aperiam.
+              {article && article.excerpt}
             </p>
           </div>
 
-          <div className="flex justify-between">
+          <div className="flex justify-between ">
             <div className="flex space-x-2">
               <button className="bg-neutral-800 hover:bg-neutral-600 transition-colors px-8 py-2 text-white font-medium text-sm">
                 EDIT
               </button>
-              <button className="border-neutral-500 hover:bg-neutral-500 hover:text-white transition-all border-1 px-8 py-2 text-neutral-600 font-medium text-sm">
-                VIEW
+              <button
+                onClick={handleDeleteArticle}
+                className="border-red-500 hover:bg-red-500 hover:text-white transition-all border-1 px-8 py-2 text-red-600 font-medium text-sm"
+              >
+                DELETE
               </button>
             </div>
 
@@ -55,7 +70,7 @@ const ViewWrittenArticle = () => {
       <div className="flex flex-col w-full lg:hidden space-y-2">
         <img
           className="h-60 lg:object-fit object-cover"
-          src="https://www.nbcsports.com/sites/rsnunited/files/article/hero/steph-curry-klay-thompson-warriors-Getty.jpg"
+          src={article && article.coverImage}
           alt="latest article"
         />
 
@@ -71,21 +86,21 @@ const ViewWrittenArticle = () => {
         </div>
 
         <h1 className="text-2xl font-spratRegular">
-          What's going on in Dub Nation?
+          {article && article.title}
         </h1>
         <p className="font-robotoLight text-sm text-justify">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure fuga,
-          molestiae in perspiciatis explicabo animi laboriosam quam ipsa beatae
-          quos neque facere, totam blanditiis facilis eius ab ipsam repellendus
-          aperiam.
+          {article && article.excerpt}
         </p>
 
         <div className="flex space-x-2">
           <button className="bg-neutral-800 hover:bg-neutral-600 transition-colors px-8 py-2 text-white font-medium text-sm">
             EDIT
           </button>
-          <button className="border-neutral-500 hover:bg-neutral-500 hover:text-white transition-all border-1 px-8 py-2 text-neutral-600 font-medium text-sm">
-            VIEW
+          <button
+            onClick={handleDeleteArticle}
+            className="border-red-500 hover:bg-red-500 hover:text-white transition-all border-1 px-8 py-2 text-red-600 font-medium text-sm"
+          >
+            DELETE
           </button>
         </div>
       </div>

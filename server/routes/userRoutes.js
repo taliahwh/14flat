e56d1eb266563eec898933bb9d16cea4b7ec8620
Taliah Wharton <taliahwh23@yahoo.com>
2022-05-02 13:15:07 -0400

    update profile feature

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,12 +2,27 @@ import express from 'express';
 
 import { authMiddleware, isAdmin } from '../middleware/authMiddleware.js';
 
-import { signIn, signUp } from '../controllers/userController.js';
+import {
+  signIn,
+  signUp,
+  getUserDetails,
+  updateUserEmail,
+  updateUserPassword,
+  adminGetAnalytics,
+} from '../controllers/userController.js';
 
 const router = express.Router();
+
+router.get('/analytics', authMiddleware, isAdmin, adminGetAnalytics);
+
+router.get('/:id', authMiddleware, getUserDetails);
 
 router.post('/signin', signIn);
 
 router.post('/signup', signUp);
+
+router.put('/profile-email', authMiddleware, updateUserEmail);
+
+router.put('/profile-password', authMiddleware, updateUserPassword);
 
 export default router;

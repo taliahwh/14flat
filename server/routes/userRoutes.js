59952e9336ccdb1294d2b1cd8c4dbf6b4f9274cc
Taliah Wharton <taliahwh23@yahoo.com>
2022-05-04 15:41:59 -0400

    admin notification workflow

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -10,11 +10,17 @@ import {
   updateUserPassword,
   adminGetAnalytics,
   sendWriterRequest,
+  approveWriterRequest,
+  declineWriterRequest,
+  getNotifications,
+  deleteNotification,
 } from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.get('/analytics', authMiddleware, isAdmin, adminGetAnalytics);
+
+router.get('/notifications', authMiddleware, getNotifications);
 
 router.get('/:id', authMiddleware, getUserDetails);
 
@@ -22,10 +28,26 @@ router.post('/signin', signIn);
 
 router.post('/signup', signUp);
 
+router.post(
+  '/approve-writer-request/:id',
+  authMiddleware,
+  isAdmin,
+  approveWriterRequest
+);
+
+router.post(
+  '/decline-writer-request/:id',
+  authMiddleware,
+  isAdmin,
+  declineWriterRequest
+);
+
 router.post('/writer-request', authMiddleware, sendWriterRequest);
 
 router.put('/profile-email', authMiddleware, updateUserEmail);
 
 router.put('/profile-password', authMiddleware, updateUserPassword);
+
+router.delete('/notifications/:id', authMiddleware, deleteNotification);
 
 export default router;
